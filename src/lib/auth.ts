fb114d728711752afb641bde2edf207676e611ed
Paulo Roberto Mesquita <pauloroberto@ajtec.eu>
2025-05-29 11:36:16 -0300

    chore: aad better auth setup

---
+++ src/lib/auth.ts
@@ -0,0 +1,23 @@
+import { verification } from "./../../auth-schema";
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionsTable",
+  },
+  account: {
+    modelName: "accountsTable",
+  },
+  verification: {
+    modelName: "verificationsTable",
+  },
+});
